342df0333c6ea80462c84da4b30422acc1566321
PARKBEB <80701049+PARKBEB@users.noreply.github.com>
2021-06-18 22:36:47 +0900

    Level1_행렬의 덧셈

--- Level1_Addition/src/Solution.java
+++ Level1_Addition/src/Solution.java
@@ -1,8 +1,14 @@
 class Solution {
     public int[][] solution(int[][] arr1, int[][] arr2) {
-        int[][] arr3 = new int[arr1.length][arr1.length]; 
+        int[][] arr3 = new int[arr1.length][arr1[0].length]; 
+        
+        
+        
         for(int i=0; i<arr1.length; i++){
-           arr3[i][i] = arr1[i][i] + arr2[i][i];
+            for(int j=0; j<arr1[0].length; j++){    // 열을 가리킴
+             arr3[i][j] = arr1[i][j] + arr2[i][j];
+            }
+            
         }
         int[][] answer = arr3;
         return answer;
