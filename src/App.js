6d025764673de8e684a0cf2498d2272622013fc0
pej <dmb03140@gmail.com>
2021-09-24 16:17:27 +0900

    update!

--- src/App.js
+++ src/App.js
@@ -1,28 +1,40 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
 import './App.css';
 
-function App () {
-    const [message, setMessage] = useState("");
-    
-    useEffect(() => {
-        fetch('/api/hello')
-            .then(response => response.text())
-            .then(message => {
-                setMessage(message);
-        });
-    },[])
-    return (
+function App() {
+
+    const [id, setId] = useState([]);
+    const [name, setName] = useState([]);
+    const [team, setTeam] = useState([]);
+
+    function get() {
+        
+        const url = "/users";
+        axios.get(url)
+        .then(function(response) {
+            setName(response.data);
+            console.log("성공");
+        })
+        .catch(function(error) {
+            console.log("실패");
+        })   
+    }
+        return (
         <div className="App">
             <header className="App-header">
+                    ID: {id} NAME: {name} TEAM: {team} 
                 <h1 className="App-title">
-                    {message}
+                    <input type= "text" className= "login-input-text"  placeholder= "id"></input>
+                    <input type= "text" className= "login-input-text"  placeholder= "name"></input>
+                    <input type= "text" className= "login-input-text"  placeholder= "team"></input>
                 </h1>
-                    <button>PUT</button>
-                    <button>GET</button>
-                    <button>DELET</button>
-                    <button>POST</button>
+                    <button onClick= {get}> GET </button>
+                    <button onClick= {get}> POST </button>
+                    <button onClick= {get}> PUT </button>
+                    <button onClick= {get}> DELETE </button>
             </header>
         </div>
-    )
+        )
 }
 export default App;
