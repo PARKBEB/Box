440174613e7669ef53bffff7497e0cafe6e93413
pej <dmb03140@gmail.com>
2021-09-24 09:40:33 +0900

    update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App () {
+    const [message, setMessage] = useState("");
+    
+    useEffect(() => {
+        fetch('/api/hello')
+            .then(response => response.text())
+            .then(message => {
+                setMessage(message);
+        });
+    },[])
+    return (
+        <div className="App">
+            <header className="App-header">
+                <h1 className="App-title">
+                    {message}
+                </h1>
+                    <button>PUT</button>
+                    <button>GET</button>
+                    <button>DELET</button>
+                    <button>POST</button>
+            </header>
+        </div>
+    )
 }
-
 export default App;
