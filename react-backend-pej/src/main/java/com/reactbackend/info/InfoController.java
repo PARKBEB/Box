1022136ab71d71b571e4da25190362e167db2838
pej <dmb03140@gmail.com>
2021-09-27 16:22:38 +0900

    First

---
+++ react-backend-pej/src/main/java/com/reactbackend/info/InfoController.java
@@ -0,0 +1,32 @@
+package com.reactbackend.info;
+
+import java.util.ArrayList;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class InfoController{
+
+	ArrayList<String> user_list = new ArrayList<>();
+	
+	@GetMapping("/users/get")
+	public ArrayList<String> userGet(){
+		return user_list;
+	}
+	
+	@PostMapping("/users/post")
+	public ArrayList<String> userPost(@RequestParam("id")String id,@RequestParam("name")String name,@RequestParam("team")String team) {
+		
+		System.out.print("도착은 함");
+		System.out.print(user_list);
+		
+		user_list.add(id);
+		user_list.add(name);
+		user_list.add(team);
+		
+		return user_list;
+	}
+}
