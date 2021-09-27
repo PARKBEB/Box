fba05fc1a4c136e516a3aa9bf0ad2bc7d5205fc1
pej <dmb03140@gmail.com>
2021-09-27 18:03:00 +0900

    post

--- react-backend-pej/src/main/java/com/reactbackend/info/InfoController.java
+++ react-backend-pej/src/main/java/com/reactbackend/info/InfoController.java
@@ -14,18 +14,21 @@ public class InfoController{
 	
 	@GetMapping("/users/get")
 	public ArrayList<String> userGet(){
+		
+		System.out.print(user_list);
+	
 		return user_list;
 	}
 	
 	@PostMapping("/users/post")
 	public ArrayList<String> userPost(@RequestParam("id")String id,@RequestParam("name")String name,@RequestParam("team")String team) {
-		
+			
 		user_list.add(id);
 		user_list.add(name);
 		user_list.add(team);
 		
-		System.out.print(user_list);
-		
+		System.out.print("아이디=" +id + "이름=" +name + "팀=" +team);
+			
 		return user_list;
 	}
 }
