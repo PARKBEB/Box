28692cc26b78d314730b99fc0f9bf583a3656250
pej <dmb03140@gmail.com>
2021-09-27 23:20:56 +0900

    post

--- src/App.js
+++ src/App.js
@@ -3,9 +3,7 @@ import './App.css';
 import React, {useState} from 'react';
 
 function App() {
-
-  const [user, setUser] = useState([]);
-
+  const [uer, setUser] =useState([]);
   const [inputs, setInputs] = useState({
       id: "",
       name: "",
@@ -14,14 +12,14 @@ function App() {
 
   const { id, name, team } = inputs;
 
-  const onChange = (e) => {
+  const onChange = (e) => {                   
       const { value, name } = e.target;
       setInputs({
         ...inputs,
         [name]: value,
       });
     };
-    
+
   /*const [id, setId]     = useState([]);
     const [name, setName] = useState([]);
     const [team, setTeam] = useState([]);
@@ -31,62 +29,48 @@ function App() {
     const url= "/users/get";
 
     axios.get(url).then(function(response) {
-      if (response.data) {
-          const templist = response.data.slice();
-          setUser(templist);
-        }
       console.log("성공");
   })
     .catch(function(error) {
-        console.log(id);
-        console.log(name);
-        console.log(team);
         console.log("실패");
     }) 
   }
 
   function post() {
 
-      const url= "/users/post";
+    const url= "/users/post";
 
-      axios.post(url, null, {
-          params : {    
-              id:   id, 
-              name: name,
-              team: team
-            }
-          })
-          .then(function(response) {
-              if (response.data) {
-                  const templist = response.data.slice();
-                  setUser(templist);
-                }
-              console.log("성공");
-          })
-          .catch(function(error) {
-              console.log(id);
-              console.log(name);
-              console.log(team);
-              console.log("실패");
-          })   
-      }
+    axios.post(url, null, {
+        params : {    
+            id:   id, 
+            name: name,
+            team: team
+          }
+        })
+        .then(function(response) {
+            console.log("성공");
+        })
+        .catch(function(error) {
+            console.log("실패");
+        })   
+    }
 
-      return (
-      <div className="App">
-          <header className="App-header">
-              ID: {id} NAME: {name} TEAM: {team} 
-              <h1 className="App-title">
-                  <input type= "text" className= "login-input-text"  name= "id"   onChange= {onChange}  placeholder= "id"></input>
-                  <input type= "text" className= "login-input-text"  name= "name" onChange= {onChange}  placeholder= "name"></input>
-                  <input type= "text" className= "login-input-text"  name= "team" onChange= {onChange}  placeholder= "team"></input>
-              </h1>
-                  <button onClick= {get}> GET </button>
-                  <button onClick= {post}> POST </button>
-                  <button onClick= {post}> PUT </button>
-                  <button onClick= {post}> DELETE </button>
-          </header>
-      </div>
-      )
+    return (
+    <div className="App">
+        <header className="App-header">
+            ID: {id}   &nbsp;&nbsp;&nbsp;&nbsp; NAME: {name} &nbsp;&nbsp;&nbsp;&nbsp; TEAM: {team} 
+            <h1 className="App-title">
+                <input type= "text" className= "login-input-text"  name= "id"   onChange= {onChange}  placeholder= "id"></input>
+                <input type= "text" className= "login-input-text"  name= "name" onChange= {onChange}  placeholder= "name"></input>
+                <input type= "text" className= "login-input-text"  name= "team" onChange= {onChange}  placeholder= "team"></input>
+            </h1>
+                <button onClick= {get}> GET </button>
+                <button onClick= {post}> POST </button>
+                <button onClick= {post}> PUT </button>
+                <button onClick= {post}> DELETE </button>
+        </header>
+    </div>
+    )
 }
 export default App;
 
