fa82e024a930bdb59e7649e1aac3c973f4c6dd69
pej <dmb03140@gmail.com>
2021-09-27 11:21:30 +0900

    update!

--- src/App.js
+++ src/App.js
@@ -4,35 +4,41 @@ import './App.css';
 
 function App() {
 
-    const [id, setId] = useState([]);
+    const [id, setId]     = useState([]);
     const [name, setName] = useState([]);
     const [team, setTeam] = useState([]);
 
-    function get() {
-        
-        const url = "/users";
-        axios.get(url)
+    function post() {
+        const url = "/users/post";
+        axios.post(url, {
+            id: {id},
+            name: {name},
+            team: {team}
+        })
         .then(function(response) {
-            setName(response.data);
             console.log("성공");
         })
         .catch(function(error) {
+            console.log(id);
+            console.log(name);
+            console.log(team);
             console.log("실패");
         })   
     }
+
         return (
         <div className="App">
             <header className="App-header">
                     ID: {id} NAME: {name} TEAM: {team} 
                 <h1 className="App-title">
-                    <input type= "text" className= "login-input-text"  placeholder= "id"></input>
-                    <input type= "text" className= "login-input-text"  placeholder= "name"></input>
-                    <input type= "text" className= "login-input-text"  placeholder= "team"></input>
+                    <input type= "text" className= "login-input-text"  name= {id}   placeholder= "id"></input>
+                    <input type= "text" className= "login-input-text"  name= {name} placeholder= "name"></input>
+                    <input type= "text" className= "login-input-text"  name= {team} placeholder= "team"></input>
                 </h1>
-                    <button onClick= {get}> GET </button>
-                    <button onClick= {get}> POST </button>
-                    <button onClick= {get}> PUT </button>
-                    <button onClick= {get}> DELETE </button>
+                    <button onClick= {post}> GET </button>
+                    <button onClick= {post}> POST </button>
+                    <button onClick= {post}> PUT </button>
+                    <button onClick= {post}> DELETE </button>
             </header>
         </div>
         )
