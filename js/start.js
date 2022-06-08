249416cd3c557d7e1b5b2140b580142fa3828080
PARKBEB <80701049+PARKBEB@users.noreply.github.com>
2022-06-08 21:44:49 +0900

    commit

--- js/start.js
+++ js/start.js
@@ -1,9 +1,16 @@
 const loginForm = document.getElementById("login-form");
-const loginInput = loginForm.querySelector("input");
-const loginButton = loginForm.querySelector("button");
+const loginInput = document.querySelector("#login-form Input");
+const greeting = document.querySelector("#greeting");
 
-function onLoginBtnClick(){
+const HIDDEN_CLASSNAME = "hidden";
+
+function onLoginSubmit(even){
+    even.preventDefault();
+    loginForm.classList.add(HIDDEN_CLASSNAME);
     const username = loginInput.value;
+    localStorage.setItem("username", username);
+    greeting.innerText = username;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
-loginButton.addEventListener("click", onLoginBtnClick);
+loginForm.addEventListener("submit", onLoginSubmit);
