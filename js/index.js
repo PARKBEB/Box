3c43c5a385b971c9ffedd205f0238db33b54abf1
EFC-090\Administrator <it.parkbeb@gmail.com>
2022-06-12 16:19:59 +0900

    profile commit

--- js/index.js
+++ js/index.js
@@ -5,3 +5,11 @@ const username = localStorage.getItem(USERNAME_KEY);
 console.log(username);
 savedName.innerText = "Hellow!"+" "+username;
 
+$(document).ready(function(){
+    $('#inputImage').change(function(){
+        let selectFile = document.querySelector("#inputImage").files[0];
+        const file = URL.createObjectURL(selectFile);
+        document.querySelector("#uploadImg").src = file;
+        
+    });
+  });
