44a262ff9fc028d3e4f26f05e4787f1deeda7a4d
PARKBEB <it.parkbeb@gmail.com>
2022-06-12 21:31:41 +0900

    commit

--- js/index.js
+++ js/index.js
@@ -1,6 +1,10 @@
 const savedName = document.querySelector(".savedName");
+const inputImg = document.querySelector("#inputImage");
+const uploadImg = document.querySelector("#uploadImage");
 const USERNAME_KEY = "username";
 const username = localStorage.getItem(USERNAME_KEY);
+
+const HIDDEN_CLASSNAME = "hidden";
 
 console.log(username);
 savedName.innerText = "Hellow!"+" "+username;
@@ -10,6 +14,7 @@ $(document).ready(function(){
         let selectFile = document.querySelector("#inputImage").files[0];
         const file = URL.createObjectURL(selectFile);
         document.querySelector("#uploadImg").src = file;
-        
+        inputImg.classList.add(HIDDEN_CLASSNAME);
+        uploadImg.classList.remove(HIDDEN_CLASSNAME);
     });
   });
