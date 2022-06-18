fbdf091b782a77309b441b88952af1de840e85b9
PARKBEB <it.parkbeb@gmail.com>
2022-06-18 12:08:49 +0900

    todo

--- js/todo.js
+++ js/todo.js
@@ -2,6 +2,14 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = toDoForm.querySelector("input");
 const toDoList = document.getElementById("todo-list");
 
+const TODOS_KEY = "todos";
+
+const toDos = [];
+
+function saveToDos(){
+    localStorage.setItem("saveToDos", JSON.stringfy(toDos));
+}
+
 function deletToDo(event){
     const li = event.target.parentElement;
     li.remove();
@@ -24,7 +32,22 @@ function handleToDoSubmit(event){
     event.preventDefault();
     const newTodo = toDoInput.value;
     toDoInput.value = "";
+    toDos.push(newTodo);
     paintToDo(newTodo);
+    saveToDos();
+
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
+
+function sayHeLLO(item){
+    console.log("this is the turn of", item);
+}
+
+const savedToDos = localStrage.getItem(TODOS_KEY);
+
+if(savedToDos !== null){
+    const parsedToDos = JSON.parse(savedToDos);
+    parseToDos.forEach(item) => console.log(""this is the turn of", item");
+}
+
